a1a9759605d6b9c3b8ea886b523a3d294b62ca03
unknown <chethika96@gmail.com>
2024-09-20 02:41:33 +0530

    Display UI and Router

---
+++ dreamstreamer/src/components/Display.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Display = () => {
+  return (
+    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-slate-700 text-white overflow-auto lg:w-[75%] lg:ml-0'>
+        
+    </div>
+  )
+}
+
+export default Display
