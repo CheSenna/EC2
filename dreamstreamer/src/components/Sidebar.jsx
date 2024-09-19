e63067813295cefd8f6c7e977494854f6827224d
unknown <chethika96@gmail.com>
2024-09-20 01:41:01 +0530

    Sidebar

--- dreamstreamer/src/components/Sidebar.jsx
+++ dreamstreamer/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import {assets} from '../assets/assets'
@@ -21,6 +22,20 @@ export const Sidebar = () => {
                     <img className='w-8' src={assets.stack_icon} alt="" />
                     <p className='font-semibold'>Your Library</p>
                 </div>
+                <div className='flex items-center gap-3'>
+                    <img className='w-5' src={assets.arrow_icon} alt="" />
+                    <img className='w-5' src={assets.plus_icon} alt='' />
+                </div>
+            </div>
+            <div className='p-4 bg-slate-700 m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                <h1>Create your first playlist!</h1>
+                <p className='font-light'>It's easy, We'll help you out</p>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create playlist</button>
+            </div>
+            <div className='p-4 bg-slate-700 m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
+                <h1>Let's find some podcasts to follow!</h1>
+                <p className='font-light'>We'll keep you updated on new episodes</p>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcasts</button>
             </div>
         </div>
     </div>
