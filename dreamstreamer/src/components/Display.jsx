a41282b228e44c359485d744cda6af7eaf4c2326
unknown <chethika96@gmail.com>
2024-09-20 14:46:33 +0530

    Home UI and albums backend

--- dreamstreamer/src/components/Display.jsx
+++ dreamstreamer/src/components/Display.jsx
@@ -1,10 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import DisplayHome from './DisplayHome'
+import DisplayAlbum from './DisplayAlbum'
 
 const Display = () => {
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-slate-700 text-white overflow-auto lg:w-[75%] lg:ml-0'>
-        
+    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-slate-800 text-white overflow-auto lg:w-[75%] lg:ml-0'>
+        <Routes>
+            <Route path='/' element={<DisplayHome/>} />
+            <Route path='/album/:id' element={<DisplayAlbum/>} />
+        </Routes>
     </div>
   )
 }
